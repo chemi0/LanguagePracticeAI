eca11739a81ddf3b5c9fe12661f3698092458578
chemi0 <milanspasojevic232@gmail.com>
2025-04-01 17:43:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import asyncio
 
 
 # Configure Google Gemini API
-genai.configure(api_key="AIzaSyAurbpVsBDTcNp7VxQ4b8DTBIWjq2_PekA")
+genai.configure(api_key="API")
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 # Translator for Japanese to English
